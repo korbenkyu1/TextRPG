9510e93519436ae07de9218ca2da67ef0d7ba110
kasajizo06 <akqnxo06@gmail.com>
2025-07-09 23:07:54 +0900

    2025-07-09

--- Assets/Demo/Player/Skills/Atttack/Ritual Severance/RitualSeverance.cs
+++ Assets/Demo/Player/Skills/Atttack/Ritual Severance/RitualSeverance.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Data/PlayerSkill/RitualSeverance")]
 public class RitualSeverance : PlayerSkillData
 {
-    [SerializeField] int scalar1;
-    [SerializeField] int scalar2;
+    [SerializeField]
+    int scalar1, scalar2;
     public override void OnActivate(CombatManager combatManager)
     {
         int damage = (int)(combatManager.player.stats.attack * scalar1 / 100f);
+        if (combatManager.player.stats.critChance >= Random.Range(1, 101))
+            damage = (int)(damage * 1.5);
         int specialDamage = (int)(combatManager.player.stats.attack * scalar2 / 100f);
         
         combatManager.enemy.damages.Add(damage);
-        combatManager.enemy.statusEffects["erode_stack"].stack -= 1;
+        combatManager.player.damages.Add(specialDamage);
+        if (--combatManager.enemy.statusEffects["erode_stack"].stack < 0)
+            combatManager.enemy.statusEffects["erode_stack"].stack = 0;
     }
 }
--- Assets/Demo/Player/Skills/Defense/Infection Rite/InfectionRite.cs
+++ Assets/Demo/Player/Skills/Defense/Infection Rite/InfectionRite.cs
@@ -5,7 +5,7 @@ public class InfectionRite: PlayerSkillData
 {
     public override void OnActivate(CombatManager combatManager)
     {
-        combatManager.player.statusEffects["evasion_chance"].stack += 1;
+        combatManager.player.statusEffects["evasion_chance"].stack++;
         combatManager.enemy.statusEffects["erode_stack"].stack += 2;
         combatManager.enemy.statusEffects["poison"].stack += (int)(combatManager.player.stats.defense * 0.3);
     }
