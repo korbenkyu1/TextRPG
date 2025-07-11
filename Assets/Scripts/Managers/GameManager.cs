37113cc9b6b0939aa4a398133ec6c9f96b05d16f
korbenkyu1 <108736444+korbenkyu1@users.noreply.github.com>
2025-07-11 23:34:21 +0900

    Save & Load

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,16 @@
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.SceneManagement;
+
+public class GameData
+{
+    public int stageIndex;
+    public int actIndex;
+    public int coin;
+    public PlayerData playerData;
+    public ResultData resultData;
+}
 
 public class GameManager : MonoBehaviour
 {
@@ -18,7 +30,6 @@ public class GameManager : MonoBehaviour
     public PlayerData playerData;
     public EnemyData enemyData;
     public ResultData resultData;
-    // reward
 
     void Awake()
     {
@@ -28,5 +39,44 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else Destroy(gameObject);
+    }
+
+    public void Save()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/TextRPG.sav";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        GameData data = new GameData();
+        data.stageIndex = stageIndex;
+        data.actIndex = actIndex;
+        data.coin = coin;
+        data.playerData = playerData;
+        data.resultData = resultData;
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+    public void Load()
+    {
+        string path = Application.persistentDataPath + "/TextRPG.sav";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = formatter.Deserialize(stream) as GameData;
+            stageIndex = data.stageIndex;
+            actIndex = data.actIndex;
+            coin = data.coin;
+            playerData = data.playerData;
+            resultData = data.resultData;
+            SceneManager.LoadScene("ActScene");
+        }
+        else
+        {
+            Debug.Log("Save file not found.");
+        }
+
     }
 }
